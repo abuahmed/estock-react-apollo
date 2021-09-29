f818c214d4fe4ccd1ffddf04ec6699bddaa20c01
abuahmed <ibrayas@gmail.com>
2021-09-29 19:33:53 +0300

    added daily sales summary bar chart to dashboard

--- src/components/dashboard/PurchaseSaleBar.tsx
+++ src/components/dashboard/PurchaseSaleBar.tsx
@@ -1,133 +1,94 @@
 import { Bar } from "react-chartjs-2";
 import {
-  Box,
-  Button,
   Card,
   CardContent,
   CardHeader,
   Divider,
   useTheme,
+  Grid,
   colors,
 } from "@material-ui/core";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-// import ArrowRightIcon from "@material-ui/icons/ArrowRight";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+  GetDailyTransactions,
+  selectTransactions,
+} from "../../features/transactions/transactionsSlice";
+import { useEffect, useState } from "react";
+import { TransactionType } from "../../features/transactions/types/transactionTypes";
 
-interface PropTypes {
-  height?: number;
-}
-
-export const PurchaseSaleBar = (props: PropTypes) => {
+export const PurchaseSaleBar = () => {
   const theme = useTheme();
+  const dispatch = useAppDispatch();
+  const { dailySalesSummary, dailyPurchasesSummary } =
+    useAppSelector(selectTransactions);
+  const [salesValues, setSalesValues] = useState<number[]>([]);
+  const [salesLabels, setSalesLabels] = useState<string[]>([]);
+  const [purchasesValues, setPurchasesValues] = useState<number[]>([]);
+  const [purchasesLabels, setPurchasesLabels] = useState<string[]>([]);
+  useEffect(() => {
+    dispatch(GetDailyTransactions(TransactionType.Sale));
+    dispatch(GetDailyTransactions(TransactionType.Purchase));
+  }, []);
+
+  useEffect(() => {
+    if (dailySalesSummary.length > 0) {
+      setSalesValues(dailySalesSummary.map((t) => t.totalAmount as number));
+      setSalesLabels(dailySalesSummary.map((t) => t.transactionDate as string));
+    }
+    if (dailyPurchasesSummary.length > 0) {
+      setPurchasesValues(
+        dailyPurchasesSummary.map((t) => t.totalAmount as number)
+      );
+      setPurchasesLabels(
+        dailyPurchasesSummary.map((t) => t.transactionDate as string)
+      );
+    }
+  }, [dailySalesSummary, dailyPurchasesSummary]);
 
   const data = {
     datasets: [
       {
-        backgroundColor: colors.green[300],
-        data: [18, 5, 19, 27, 29, 19, 20],
+        backgroundColor: theme.palette.primary.main,
+        data: salesValues,
         label: "Purchase",
-      },
-      {
-        backgroundColor: colors.green[900],
-        data: [11, 20, 12, 29, 30, 25, 13],
-        label: "Sale",
+        borderWidth: 8,
+        borderColor: colors.common.white,
+        hoverBorderColor: colors.common.white,
       },
     ],
-    labels: ["1 Aug", "2 Aug", "3 Aug", "4 Aug", "Yesterday", "Today"],
+    labels: salesLabels,
   };
 
-  const options = {
-    animation: false as boolean,
-    cornerRadius: 20,
-    layout: { padding: 0 },
-    legend: { display: false },
-    maintainAspectRatio: false,
-    responsive: true,
-    scales: {
-      xAxes: [
-        {
-          barThickness: 12,
-          maxBarThickness: 10,
-          barPercentage: 0.5,
-          categoryPercentage: 0.5,
-          ticks: {
-            fontColor: theme.palette.text.secondary,
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-        },
-      ],
-      yAxes: [
-        {
-          ticks: {
-            fontColor: theme.palette.text.secondary,
-            beginAtZero: true,
-            min: 0,
-          },
-          gridLines: {
-            borderDash: [2],
-            borderDashOffset: [2],
-            color: theme.palette.divider,
-            drawBorder: false,
-            zeroLineBorderDash: [2],
-            zeroLineBorderDashOffset: [2],
-            zeroLineColor: theme.palette.divider,
-          },
-        },
-      ],
-    },
-    tooltips: {
-      backgroundColor: theme.palette.background.paper,
-      bodyFontColor: theme.palette.text.secondary,
-      borderColor: theme.palette.divider,
-      borderWidth: 1,
-      enabled: true,
-      footerFontColor: theme.palette.text.secondary,
-      intersect: false,
-      mode: "index",
-      titleFontColor: theme.palette.text.primary,
-    },
+  const data2 = {
+    datasets: [
+      {
+        backgroundColor: theme.palette.secondary.main,
+        data: purchasesValues,
+        label: "Sale",
+        borderWidth: 8,
+        borderColor: colors.common.white,
+        hoverBorderColor: colors.common.white,
+      },
+    ],
+    labels: purchasesLabels,
   };
 
   return (
-    <Card {...props}>
-      <CardHeader
-        action={
-          <Button endIcon={<ArrowDropDownIcon />} size="small" variant="text">
-            Last 7 days
-          </Button>
-        }
-        title="Purchase/Sale Progress"
-      />
-      <Divider />
+    <Card sx={{ height: "100%" }}>
       <CardContent>
-        <Box
-          sx={{
-            height: 400,
-            position: "relative",
-          }}
-        >
-          <Bar data={data} />
-        </Box>
+        <Grid container spacing={2}>
+          <Grid item md={6} xs={12}>
+            <CardHeader title={`Last 10 Day Purchases`} />
+            <Divider />
+            <Bar data={data} />
+          </Grid>
+          <Grid item md={6} xs={12}>
+            <CardHeader title={`Last 10 Day Sales`} />
+            <Divider />
+            <Bar data={data2} />
+          </Grid>
+        </Grid>
       </CardContent>
-      {/* <Divider />
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          p: 2,
-        }}
-      >
-        <Button
-          color="primary"
-          endIcon={<ArrowRightIcon />}
-          size="small"
-          variant="text"
-        >
-          Overview
-        </Button>
-      </Box> */}
     </Card>
   );
 };
