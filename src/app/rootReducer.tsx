82fa103e00cb7294eab080da0fb7a962debdfb93
abuahmed <ibrayas@gmail.com>
2021-10-26 17:33:03 +0300

    Removed Counter Slice

--- src/app/rootReducer.tsx
+++ src/app/rootReducer.tsx
@@ -1,7 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 // Reducers
-import counterReducer from "../features/counter/counterSlice";
 import authReducer from "../features/auth/authReducers";
 import usersReducer from "../features/users/usersSlice";
 import settingsReducer from "../features/settings/settingsSlice";
@@ -9,7 +8,6 @@ import setupsReducer from "../features/setups/setupSlices";
 import transactionsReducer from "../features/transactions/transactionsSlice";
 
 const rootReducer = combineReducers({
-  counter: counterReducer,
   auth: authReducer,
   users: usersReducer,
   setups: setupsReducer,
